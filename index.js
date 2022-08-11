63541e47bd749dd674dd997706b0726fbeab0d58
Amir Anglo <angloamir@gmail.com>
2022-08-11 17:05:19 +0200

    Gen key upgrade, useKey added

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
-const fs = require('fs')
+import fs from 'fs'
+const fileJson = './key.json'
+const data = JSON.parse(fs.readFileSync(fileJson));
 
-function genKey(num) {
-    var key = "";
-    for(let i = 0; i < num; i++) {
-        const char = String.fromCharCode((Math.random() * (126 - 33) + 33));
-        var key = key + char
-    };
-    fs.writeFileSync('./mdp.json', JSON.stringify({key: key, length: num}));
-    return key;
+function writeFile(key, content) {
+    data.push({key: key, content: content});
+    fs.writeFileSync(fileJson, JSON.stringify(data, null, 4));    
 }
 
-genKey(64)
+function genKey(content) {
+    let key = "";
+    for(let i = 0; i < 8; i++) {
+        if(i % 2 === 0) {
+            key.split("").reverse().join("");
+        }
+        key += String.fromCharCode(Math.floor(Math.random() * (121 - 33) + 33));
+    };
+
+    key.split("").reverse().join(""); 
+    return writeFile(key, content);
+}
+
+function useKey(value) {
+    if(data.find(e => e.key === value)) {
+        const indexKey = data.find(e => e.key === value)
+        const content = indexKey.content
+        indexKey.key = '';
+        indexKey.content = '';
+        fs.writeFileSync(fileJson, JSON.stringify(data, null, 4));      
+
+        return content;
+    }
+
+}
+
